226fd2aaaa29005faa596403e91068a189341d45
jared w <jaredtimothywillis@gmail.com>
2017-12-30 11:35:00 -0800

    menu assignment complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -9,47 +9,52 @@ class MenuController
 
   def main_menu
     # #2
-    puts "Main Menu - #{address_book.entries.count} entries"
-    puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
-    print "Enter your selection: "
+      puts "Main Menu - #{address_book.entries.count} entries"
+      puts "1 - View all entries"
+      puts "2 - Create an entry"
+      puts "3 - Search for an entry"
+      puts "4 - Import entries from a CSV"
+      pust "5 - View Entry n"
+      puts "6 - Exit"
+      print "Enter your selection: "
 
-    # #3
-    selection = gets.to_i
-    case selection
-  when 1
-    system "clear"
-    view_all_entries
-    main_menu
-  when 2
-    system "clear"
-    create_entry
-    main_menu
-  when 3
-    system "clear"
-    search_entries
-    main_menu
-  when 4
-    system "clear"
-    read_csv
-    main_menu
-  when 5
-    puts "Good-bye!"
-    # #8
-    exit(0)
-  # #9
-  else
-    system "clear"
-    puts "Sorry, that is not a valid input"
-    main_menu
+      # #3
+      selection = gets.to_i
+      case selection
+    when 1
+      system "clear"
+      view_all_entries
+      main_menu
+    when 2
+      system "clear"
+      create_entry
+      main_menu
+    when 3
+      system "clear"
+      search_entries
+      main_menu
+    when 4
+      system "clear"
+      read_csv
+      main_menu
+    when 5
+      system "clear"
+      entry_n_submenu
+      main_menu
+    when 6
+      puts "Good-bye!"
+      # #8
+      exit(0)
+    # #9
+    else
+      system "clear"
+      puts "Sorry, that is not a valid input"
+      main_menu
+      end
     end
-  end
 
     # #10
-    def view_all_entries
+  def view_all_entries
       address_book.entries.each do |entry|
        system "clear"
        puts entry.to_s
@@ -61,7 +66,7 @@ class MenuController
      puts "End of entries"
     end
 
-    def create_entry
+  def create_entry
       system "clear"
       puts "New AddressBloc Entry"
       # #12
@@ -79,7 +84,7 @@ class MenuController
       puts "New entry created"
     end
 
-    def search_entries
+  def search_entries
        puts "n - next entry"
        puts "d - delete entry"
        puts "e - edit this entry"
@@ -103,7 +108,22 @@ class MenuController
            puts "#{selection} is not a valid input"
            entry_submenu(entry)
        end
-    
+
+    end
+
+    def entry_n_submenu
+      prints "Entry number to view: "
+      selection = gets.chomp.to_i
+
+      if selection < @address_book.entries.count
+        puts @address_book.entries[selection]
+        puts "Press Enter to return to main menu."
+        gets.chomp
+        system "clear"
+      else
+        puts "#{selection} is not a valid entry."
+        entry_n_submenu
+      end
     end
 
     def read_csv
